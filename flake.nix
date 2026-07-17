cce472622e9f55f3fb16226ae6d2a4ada803c9ab
Daru <mica.tui@proton.me>
2026-07-17 19:43:20 +0200

    feat(nix): add flake

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "Tracing back one's origin, the crabby way.";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+      perSystem =
+        {
+          pkgs,
+          ...
+        }:
+        {
+          devShells.default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              cargo
+              rustc
+              rustfmt
+            ];
+            RUST_SRC_PATH = pkgs.rustPlatform.rustLibSrc;
+          };
+          packages.default = pkgs.hello;
+        };
+    };
+}
