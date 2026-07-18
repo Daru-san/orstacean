76aa54d6b776bfbda62a75891bb8b7fe6e941c7f
Daru <mica.tui@proton.me>
2026-07-18 21:57:31 +0200

    feat(dashboard): add border renderer

--- src/app/dashboard.rs
+++ src/app/dashboard.rs
@@ -1,4 +1,4 @@
-use ratatui::Frame;
+use ratatui::{Frame, layout::Rect, widgets::Block};
 
 pub struct Dashboard {
     page: usize,
@@ -11,7 +11,10 @@ impl Default for Dashboard {
 }
 
 impl Dashboard {
-    pub fn render(_frame: &mut Frame) {
+    pub fn render(_frame: &mut Frame) {}
+}
 
-    }
+fn render_border(frame: &mut Frame<'_>, area: Rect) {
+    let block = Block::bordered().title("Ferris");
+    frame.render_widget(block, area);
 }
