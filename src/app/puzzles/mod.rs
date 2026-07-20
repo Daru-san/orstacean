111ec2a6cd3c58b0f77cf11f65bbf05a13fc10ea
Daru <mica.tui@proton.me>
2026-07-20 17:04:03 +0200

    refactor(puzzles): store puzzles as Rc<RefCell<T>>

--- src/app/puzzles/mod.rs
+++ src/app/puzzles/mod.rs
@@ -1,3 +1,4 @@
+use std::cell::RefCell;
 use std::rc::Rc;
 
 use ratatui::Frame;
@@ -17,14 +18,14 @@ mod word_match;
 pub struct PuzzleView {
     active_puzzle: Puzzle,
     results: Vec<String>,
-    puzzle: Rc<dyn IPuzzle>,
+    puzzle: Rc<RefCell<dyn IPuzzle>>,
 }
 
 enum Puzzle {
-    Tile1(Rc<PaneGrid>),
-    Tile2(Rc<PaneGrid>),
+    Tile1(Rc<RefCell<PaneGrid>>),
+    Tile2(Rc<RefCell<PaneGrid>>),
     Riddle,
-    Maze(Rc<Maze>),
+    Maze(Rc<RefCell<Maze>>),
     Cipher,
 }
 
@@ -37,7 +38,7 @@ pub trait IPuzzle {
 
 impl PuzzleView {
     pub fn new() -> Self {
-        let grid = Rc::new(PaneGrid::new("CRAB"));
+        let grid = Rc::new(RefCell::new(PaneGrid::new("CRAB")));
         let puzzle = Puzzle::Tile1(grid.clone());
         Self {
             active_puzzle: puzzle,
@@ -50,7 +51,9 @@ impl PuzzleView {
         let layout = Layout::vertical([Percentage(100), Min(1)]);
         let [main_area, bottom_area] = area.layout(&layout);
 
-        let help = self.puzzle.keys_hints();
+        let mut puzzle = self.puzzle.borrow_mut();
+
+        let help = puzzle.keys_hints();
 
         help.render(bottom_area, frame.buffer_mut());
     }
