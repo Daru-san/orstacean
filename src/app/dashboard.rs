6f6694210ca6e7e202565779ee6646eeaf3aa521
Daru <mica.tui@proton.me>
2026-07-20 14:11:46 +0200

    feat(dashboard): add audio support

--- src/app/dashboard.rs
+++ src/app/dashboard.rs
@@ -1,21 +1,21 @@
-use std::fmt::Display;
+use std::cell::Cell;
+use std::rc::Rc;
 
 use ratatui::layout::Constraint::{Length, Min, Percentage};
 use ratatui::layout::Layout;
-use ratatui::style::{Modifier, Style, Stylize};
+use ratatui::style::{Color, Modifier, Style, Stylize};
 use ratatui::text::{Line, Span};
-use ratatui::widgets::{Paragraph, StatefulWidget, Widget};
+use ratatui::widgets::{Paragraph, Widget};
 use ratatui::{Frame, widgets::Block};
 
 use crate::APP_NAME;
 use crate::app::chat_box::ChatBox;
 
-const SCROLLVIEW_HEIGHT: u16 = 100;
-
-#[derive(Debug)]
 pub struct Dashboard {
     chatbox: ChatBox,
     stage: Stage,
+    mixer: rodio::mixer::Mixer,
+    volume: Rc<Cell<f32>>,
 }
 
 #[derive(Debug, PartialEq, Copy, Clone)]
@@ -49,38 +49,58 @@ fn age_suffix(age: u8) -> &'static str {
 }
 
 impl Dashboard {
-    pub fn introduce_puzzles(&mut self, name: impl Into<String> + Display, age: u8) {
+    pub fn new(mixer: rodio::mixer::Mixer, volume: Rc<Cell<f32>>) -> Self {
+        Self {
+            chatbox: ChatBox::new(&[], mixer.clone(), volume.clone()),
+            mixer,
+            stage: Stage::Greeting,
+            volume,
+        }
+    }
+    pub fn introduce_puzzles(&mut self, name: impl AsRef<str>, age: u8) {
         self.stage = Stage::PuzzleIntro;
-        self.chatbox = ChatBox::new(&[
-            format!("Irrashai {name}. Good to meet you."),
-            format!("Today, we're going to celebrate you {age}th birthday!"),
-            String::from("How are we going to do so, you ask?"),
-            String::from("Torture of course. It's only natural."),
-            String::from(
-                "You will be given a set of puzzles to complete, each one getting you closer to the end... your end that is.",
-            ),
-            String::from(
-                "Each puzzle will give you sufficient instructions for completion. They're so simple you can't fail.",
-            ),
-            String::from("If you do fail however, I'm not sure I'll be able to let it slide."),
-            String::from("In any case. You should get started now. You don't have much time."),
-            String::from("You will be taken to puzzle number #1."),
-            String::from("準備はできたか？"),
-        ]);
+        self.chatbox = ChatBox::new(
+            &[
+                format!("Irrashai {}.", name.as_ref()),
+                String::from("Nice to meet you."),
+                format!(
+                    "Today, we're going to celebrate you {age}{} birthday!",
+                    age_suffix(age)
+                ),
+                String::from("How are we going to do so, you ask?"),
+                String::from("Torture of course. It's only natural."),
+                String::from(
+                    "You will be given a set of puzzles to complete, each one getting you closer to the end.",
+                ),
+                String::from("..."),
+                String::from("Closer to your end that is."),
+                String::from("Each puzzle will give you sufficient instructions for completion."),
+                String::from("They're so simple you can't fail."),
+                String::from("..."),
+                String::from("If you do fail however, I'm not sure I'll be able to let it slide."),
+                String::from("In any case. You should get started now. You don't have much time."),
+                String::from("You will be taken to puzzle number #1."),
+                String::from("準備はできたか？"),
+            ],
+            self.mixer.clone(),
+            self.volume.clone(),
+        );
     }
 
     pub fn greet(&mut self) {
         self.stage = Stage::Greeting;
-        self.chatbox = ChatBox::new(&[
-            String::from("Welcome to Oricrabby."),
-            String::from("My name is Ferris."),
-            String::from("You may already know me as the Rust mascot."),
-            String::from("As you can see, I am a crab."),
-            String::from(
-                "You shall join our kind soon enough. That however, is a story for another day.",
-            ),
-            String::from("Let's start simple. Why don't you introduce yourself?"),
-        ]);
+        self.chatbox = ChatBox::new(
+            &[
+                format!("Welcome to {APP_NAME}."),
+                String::from("My name is Ferris."),
+                String::from("You may already know me as the Rust mascot."),
+                String::from("As you can see, I am a crab."),
+                String::from("You shall join our kind soon enough."),
+                String::from("Let's start simple. Why don't you introduce yourself?"),
+            ],
+            self.mixer.clone(),
+            self.volume.clone(),
+        );
     }
 
     pub fn render(&mut self, frame: &mut Frame) {
